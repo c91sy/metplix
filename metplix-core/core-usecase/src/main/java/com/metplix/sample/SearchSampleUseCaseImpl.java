5852ef8d426b616874ce70267d3e04e5e5e7eae0
(Seyoung Choi) <(c91sy@naver.com)>
2025-02-02 23:57:38 +0900

    Spring Boot 3.4 이후부터는 서비스 구현체를 빈으로 등록하기 위해 @Service을 사용해야 함 (인터페이스 만으로 빈 등록 X)

---
+++ metplix-core/core-usecase/src/main/java/com/metplix/sample/SearchSampleUseCaseImpl.java
@@ -0,0 +1,21 @@
+package com.metplix.sample;
+
+import com.metplix.sample.response.SampleResponse;
+import org.springframework.stereotype.Service;
+
+@Service  // 빈으로 등록
+public class SearchSampleUseCaseImpl implements SearchSampleUseCase {
+
+    @Override
+    public SampleResponse getSample() {
+        // 예시 응답
+        return new SampleResponse("Sample");
+    }
+}
+
+/*
+Spring Boot 3.4 이후부터는 서비스 구현체를 빈으로 등록하기 위해 @Service을 사용해야 합니다.
+이전 버전에서는 인터페이스만으로도 빈이 등록되는 경우가 있었지만,
+Spring Boot 3.4에서는 인터페이스만으로는 자동 빈 등록이 되지 않고,
+구현체 클래스를 명시적으로 빈으로 등록해야 합니다.
+*/
