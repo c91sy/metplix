eccf760f967bf3e028ef11ed841dbee28d7af779
(Seyoung Choi) <(c91sy@naver.com)>
2025-03-26 18:19:48 +0900

    스프링시큐리티 설정

--- metplix-apps/app-api/build.gradle.kts
+++ metplix-apps/app-api/build.gradle.kts
@@ -1,17 +1,19 @@
 dependencies {
     implementation(project(":metplix-core:core-usecase"))
     implementation(project(":metplix-core:core-service"))
+
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.security:spring-security-oauth2-client")
+    implementation("org.springframework.data:spring-data-commons")
 }
 
-/*
-runtimeOnly(project(":metplix-core:core-service"))
-유스케이스 모듈은 서비스 모듈의 구현체를 직접 참조하지 않으며,
-서비스 모듈에 대한 의존성을 추가하되, 런타임에만 필요하도록 설정한다.
-이렇게 하면 유스케이스가 서비스의 인터페이스에만 의존하게 되어 결합도를 낮출 수 있다.
-
-초기 테스트 단계에서는 런타임 의존성을 사용하여 유스케이스와 서비스 간의 상호작용을 검증할 수 있다.
-하지만 본격적으로 프로젝트를 진행할 때는 서비스 모듈을 `implementation`으로 변경하여,
-유스케이스가 서비스의 구현체를 컴파일 타임에도 참조할 수 있도록 해야 한다.
-이렇게 하면 전체 애플리케이션의 구조가 명확해지고, 의존성 관리가 용이해진다.
-*/
+val appMainClassName = "com.metplix.MetplixApiApplication"
+tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    mainClass.set(appMainClassName)
+    archiveClassifier.set("boot")
+}
---
+++ metplix-apps/app-api/src/main/java/com/metplix/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.metplix.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
+
+
+@Configuration
+@EnableWebSecurity //웹 보안을 설정하는 어노테이션 HTTP 요청에 대한 보안 설정을 담당
+@EnableMethodSecurity //메서드 보안을 설정, 서비스나 비즈니스 로직 계층에서 메서드 수준에서의 인증 및 인가를 제어
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(AbstractHttpConfigurer::disable);
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
+
+        http.authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests.anyRequest().authenticated());  //요청에 대한 인가 설정
+
+        http.oauth2Login(oauth2 ->
+                oauth2.failureUrl("/login?error=true")
+        );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.setAllowedHeaders(Collections.singletonList("*"));
+            configuration.setAllowedMethods(Collections.singletonList("*"));
+            configuration.setAllowedOriginPatterns(Collections.singletonList("*"));
+            configuration.setAllowCredentials(true);
+            return configuration;
+        };
+    }
+}
+
+/*
+JWT (JSON Web Token) 또는 OAuth2와 같은 다른 인증 방식을 사용하려는 경우
+기본적으로 제공되는 인증 방식인 HTTP Basic Authentication, 폼 로그인, CSRF 보호를 비활성화하는 것이 일반적
+단
+클라이언트와 서버 간의 통신을 가능하게 하고, 보안을 강화하기 위해 CORS를 활성화하는 것이 중요.
+이는 인증 방식과는 별개로, API 접근을 허용하기 위한 설정
+CorsConfigurationSource 메서드를 만든 이유는 CORS 설정을 중앙에서 관리하고 재사용할 수 있도록 하기 위해서
+*/
